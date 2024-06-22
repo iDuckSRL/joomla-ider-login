5e14939e1b217b00d461cd4395e2d24aa92bb59a
iDuck SRL <plugins@jlm.srl>
2024-06-22 14:47:22 +0000

    chg: General info

--- includes/IDER_Helpers.php
+++ includes/IDER_Helpers.php
@@ -3,22 +3,20 @@
 defined('_JEXEC') or die;
 
 /**
- * Jlm SRL
+ * iDuck SRL
  *
  * NOTICE OF LICENSE
  *
  * This source file is subject to the EULA
  * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * https://www.ider.com/IDER-LICENSE-COMMUNITY.txt
  *
  ********************************************************************
- * @category     Jlmsrl
+ * @category     iDuckSRL
  * @package      Joomla.Plugin
  * @subpackage   System.IDer_Login
- * @author       Emanuele Coppola <plugins@jlm.srl>
- * @copyright    Copyright (c) 2016 - 2018 Jlm SRL (http://www.jlm.srl)
- * @license      https://www.ider.com/IDER-LICENSE-COMMUNITY.txt
+ * @author       Emanuele Coppola <plugins@ider.com>
+ * @copyright    Copyright (c) 2016 - 2024 iDuck SRL
+ * @license      https://github.com/iDuckSRL/joomla-ider-login
  */
 
 class IDER_Helpers
